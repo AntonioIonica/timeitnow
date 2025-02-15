63d954b9309189c1b223dc89c71e530305b6fa6c
Vntonio <antonioionica@gmail.com>
2025-02-15 20:36:28 +0200

    fixed eslint and added quote generator

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: Request) {
   try {
     const res = await fetch("https://zenquotes.io/api/random");
